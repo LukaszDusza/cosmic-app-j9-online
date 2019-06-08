e818980108d402b220862fc30eda66a468e62def
Lukasz Dusza <lukaszdusza280@gmail.com>
2019-06-08 11:50:21 +0200

    init

---
+++ src/main/java/com/CosmicAppJ9Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicAppJ9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmicAppJ9Application.class, args);
+    }
+
+}
