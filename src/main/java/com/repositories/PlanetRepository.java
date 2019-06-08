a713d8a529d70047166766a64da2ab9413f07263
Lukasz Dusza <lukaszdusza280@gmail.com>
2019-06-08 16:35:41 +0200

    create repository for planets

---
+++ src/main/java/com/repositories/PlanetRepository.java
@@ -0,0 +1,25 @@
+package com.repositories;
+
+import com.models.Planet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PlanetRepository extends JpaRepository<Planet, Long> {
+
+ // @Query(value = "select * from planets where  planet_name = ?1", nativeQuery = true) //SQL
+    @Query("select p from Planet p where p.planetName = ?1") //JPQL
+    Optional<Planet> findPlanetByName(String planetName);
+
+    @Query("select p from Planet p where " +
+            "p.planetName like %?1% " +
+            "or " +
+            "p.planetInfo like %?1% " +
+            "or " +
+            "p.planetType like %?1%") //JPQL
+    List<Planet> findPlanetsByParam(String param);
+}
